64a83a5226fc2bc9aeb9a4ac73cf58a878ef7aaf
Tyler Chan <tylerchan28@gmail.com>
2021-11-08 12:39:05 -0800

    Add database schema

---
+++ database/index.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+});
+
+client.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connected to PostgreSQL');
+  }
+});
+
+module.exports = client;
