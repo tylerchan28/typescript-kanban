559205963351289b2ae7cab2d0b355038b9eed0b
Tyler Chan <tylerchan28@gmail.com>
2021-11-10 16:33:48 -0800

    Fix Google OAuth to access user info

--- pages/api/server.js
+++ pages/api/server.js
@@ -4,12 +4,23 @@ const path = require('path');
 const { Client } = require('pg');
 const next = require('next');
 const passport = require('passport');
+const session = require("express-session");
+
+
 
 const server = express();
 const port = 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+// See request info
+// server.use((req, res, next) => {
+//   console.log(req.url);
+//   console.log(req.body);
+//   console.log(req.method);
+//   next();
+// })
 
 app.prepare().then(() => {
   const client = new Client({
@@ -28,36 +39,17 @@ app.prepare().then(() => {
   });
  
   // Google Authentication
-  var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
-  // server.use(passport.initialize())
-  // server.use(passport.session())
-  
-  // passport.serializeUser(function(user, done) {;
-  //   return done(null, user.id);
-  // });
-  // passport.serializeUser(function(id, done) {
-  //   console.log('Serialize user called.');
-  //   return done(null, { User: user });
-  // });
+  const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+  server.use(session({ secret: "cats", resave: true, saveUninitialized: true }));
+  server.use(passport.initialize())
+  server.use(passport.session())
 
-  let userProfile;
-
-
-  passport.serializeUser(function(user, cb) {
-    cb(null, user);
+  passport.serializeUser(function(user, done) {
+    done(null, user);
   });
-  passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
   });
-
-  // passport.deserializeUser(function(id, done) {
-  //   console.log('Deserialize user called.');
-  //   return done(null, { firstName: 'Foo', lastName: 'Bar' });
-  // });
-
-  // passport.deserializeUser(function(id, done) {
-  //   return done(null, user.id);
-  // });
 
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -66,36 +58,27 @@ app.prepare().then(() => {
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
-      userProfile=profile;
-      return done(null, userProfile);
-    ;
+      return done(null, profile);
   }
   ));
 
-  // server.use(cors({
-  //   origin: "http://localhost:3000",
-  //   credentials: true,
-  // }))
-
-
   server.get('/auth/google',
     passport.authenticate('google', { scope:
-      [ 'email', 'profile' ] }
+      [ 'openid', 'email', 'profile' ] }
   ));
 
-  server.get('/auth/google/callback', (req, res) => {
-    passport.authenticate('google', { failureRedirect: '/error' }, res.redirect('/success')
-  )});
-    // passport.authenticate( 'google', 
-    // { failureRedirect: '/failure' },
-    // function(req, res) {
-    //   res.json(userProfile)
-    // }
-    // ));
-
+  server.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/error',
+    }),
+    function (req, res) {
+        res.redirect('/success')
+    }
+  )
+    
   server.get("/success", (req, res) => {
-    res.json('success');
-    console.log('success');
+    res.json(`Success ${req.user.email}`)
+    console.log(req.user);
   })
   
   server.get('/error', (req, res) => res.json("error logging in"));
