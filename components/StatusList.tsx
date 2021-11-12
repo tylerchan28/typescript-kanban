566edd9d5ff510811c5a7fdd9c712569efa32636
Tyler Chan <tylerchan28@gmail.com>
2021-11-12 15:50:29 -0800

    Start rework of data flow

--- components/StatusList.tsx
+++ components/StatusList.tsx
@@ -1,11 +1,35 @@
+import { Droppable } from "react-beautiful-dnd";
+import styles from "../styles/TodoCard.module.css";
+import TodoCard from "./TodoCard";
 
+type Props = {
+    project_name: string
+    list_id: number
+}
 
-function StatusList() {
+function StatusList({ project_name, list_id }: Props) {
     return (
-        <div>
-            hello
-        </div>
+        <Droppable droppableId={project_name}>
+                  {(provided) => 
+                    <div className={styles.todos} {...provided.droppableProps} ref={provided.innerRef}>
+                        <div>Completed</div>
+                        {cards.map((todo, index) => (
+                            <TodoCard todo={todo} key={todo.id} index={index}/>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                  }
+        </Droppable>
     )
 }
 
+export const getServerSideProps = () => {
+    // Within this list, GET all the cards associated with the list_id
+    // cards will be added to props and then mapped (maybe useState as well)
+
+}
+
 export default StatusList
+
+
+// map the todo cards under here (will then be called under [projectId].tsx)
--- pages/project/[projectId].tsx
+++ pages/project/[projectId].tsx
@@ -6,153 +6,181 @@ import { Project } from "../../models/project-model";
 import TodoCard from "../../components/TodoCard";
 import { DragDropContext, DraggableLocation, Droppable, DropResult, resetServerContext } from "react-beautiful-dnd";
 import { Card } from "../../models/card-model";
+const { Client } = require("pg");
 
 
 interface Props {
   project: Project;
+  lists: any
 }
 
-const Project = ({ project }: Props) => {
+const Project = ({ project, lists }: Props) => {
   const router = useRouter();
   const { projectId } = router.query;
-  const [todos, updateTodos] = useState(project.todo)
-  const [inProgress, updateInProgress] = useState(project.inProgress)
-  const [completed, updateCompleted] = useState(project.completed)
+  // const [todos, updateTodos] = useState(project.todo)
+  // const [inProgress, updateInProgress] = useState(project.inProgress)
+  // const [completed, updateCompleted] = useState(project.completed)
 
-  const getList = (id: string) => {
-    if (id === "todos") {
-        return todos;
-    } 
-    if (id === "in-progress") {
-        return inProgress
-    }
-    if (id === "completed") {
-        return completed
-    }
-  }
+  // const getList = (id: string) => {
+  //   if (id === "todos") {
+  //       return todos;
+  //   } 
+  //   if (id === "in-progress") {
+  //       return inProgress
+  //   }
+  //   if (id === "completed") {
+  //       return completed
+  //   }
+  // }
+  
+  // const reorderSection = (list: Card[] | undefined, startIndex: number, endIndex: number) => {
+  //   const result = Array.from(list!);
+  //   const [removed] = result.splice(startIndex, 1);
+  //   result.splice(endIndex, 0, removed);
+  //   return result;
+  // }
 
-  const reorderSection = (list: Card[] | undefined, startIndex: number, endIndex: number) => {
-    const result = Array.from(list!);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    return result;
-  }
+  // const move = (source: Card[] | undefined, destination: Card[] | undefined, droppableSource: DraggableLocation, droppableDestination: DraggableLocation) => {
+  //   const sourceClone = Array.from(source!);
+  //   const destinationClone = Array.from(destination!);
+  //   const [removed] = sourceClone.splice(droppableSource.index, 1);
 
-  const move = (source: Card[] | undefined, destination: Card[] | undefined, droppableSource: DraggableLocation, droppableDestination: DraggableLocation) => {
-    const sourceClone = Array.from(source!);
-    const destinationClone = Array.from(destination!);
-    const [removed] = sourceClone.splice(droppableSource.index, 1);
+  //   destinationClone.splice(droppableDestination.index, 0, removed);
 
-    destinationClone.splice(droppableDestination.index, 0, removed);
-
-    const result: any = {};
-    result[droppableSource.droppableId] = sourceClone;
-    result[droppableDestination.droppableId] = destinationClone;
-    return result;
-  }
+  //   const result: any = {};
+  //   result[droppableSource.droppableId] = sourceClone;
+  //   result[droppableDestination.droppableId] = destinationClone;
+  //   return result;
+  // }
 // After every change, don't forget to update in the database
-  const handleDragEnd = (result: DropResult) => {
-    const { destination, source } = result;
-    if (!destination) return;
-    if (source.droppableId === destination.droppableId) {
-        switch (source.droppableId) {
-            case "todos":
-                updateTodos(reorderSection(todos, source.index, destination.index))
-                break
-            case "in-progress":
-                updateInProgress(reorderSection(inProgress, source.index, destination.index))
-                break
-            case "completed":
-                updateCompleted(reorderSection(completed, source.index, destination.index))
-                break
-        }
-    } else {
-        const result = move(getList(source.droppableId), getList(destination.droppableId), source, destination)
+  // const handleDragEnd = (result: DropResult) => {
+  //   const { destination, source } = result;
+  //   if (!destination) return;
+  //   if (source.droppableId === destination.droppableId) {
+  //       switch (source.droppableId) {
+  //           case "todos":
+  //               updateTodos(reorderSection(todos, source.index, destination.index))
+  //               break
+  //           case "in-progress":
+  //               updateInProgress(reorderSection(inProgress, source.index, destination.index))
+  //               break
+  //           case "completed":
+  //               updateCompleted(reorderSection(completed, source.index, destination.index))
+  //               break
+  //       }
+  //   } else {
+  //       const result = move(getList(source.droppableId), getList(destination.droppableId), source, destination)
         
         
         
-        const newTodos = result.todos ? result.todos : undefined;
-        const newInProgress = result["in-progress"] ? result["in-progress"] : undefined;
-        const newCompleted = result.completed ? result.completed : undefined;
-        if (newTodos === undefined) {
-            updateInProgress(newInProgress);
-            updateCompleted(newCompleted);
-        } else if (newInProgress === undefined) {
-            updateTodos(newTodos);
-            updateCompleted(newCompleted);
-        } else if (newCompleted === undefined) {
-            updateTodos(newTodos);
-            updateInProgress(newInProgress)
-        }
-    } 
-  }
+  //       const newTodos = result.todos ? result.todos : undefined;
+  //       const newInProgress = result["in-progress"] ? result["in-progress"] : undefined;
+  //       const newCompleted = result.completed ? result.completed : undefined;
+  //       if (newTodos === undefined) {
+  //           updateInProgress(newInProgress);
+  //           updateCompleted(newCompleted);
+  //       } else if (newInProgress === undefined) {
+  //           updateTodos(newTodos);
+  //           updateCompleted(newCompleted);
+  //       } else if (newCompleted === undefined) {
+  //           updateTodos(newTodos);
+  //           updateInProgress(newInProgress)
+  //       }
+  //   } 
+  // }
 
   return (
     <div>
-      <div>{project.project_name}</div>
+      {/* <div>{project.project_name}</div> */}
       <div>Project {projectId}</div>
       <div className={styles.container}>
-          <DragDropContext onDragEnd={handleDragEnd}>
-              {/* Each droppable should become its own list component with a name */}
-              <Droppable droppableId="todos">
-                  {(provided) => 
-                    <div className={styles.todos} {...provided.droppableProps} ref={provided.innerRef}>
-                        <div>To Do</div>
-                        {todos.map((todo, index) => (
-                            <TodoCard todo={todo} key={todo.id} index={index}/>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                  }
-              </Droppable>
-              <Droppable droppableId="in-progress">
-                  {(provided) => 
-                    <div className={styles.todos} {...provided.droppableProps} ref={provided.innerRef}>
-                        <div>In Progress</div>
-                        {inProgress.map((todo, index) => (
-                            <TodoCard todo={todo} key={todo.id} index={index}/>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                  }
-              </Droppable>
-              <Droppable droppableId="completed">
-                  {(provided) => 
-                    <div className={styles.todos} {...provided.droppableProps} ref={provided.innerRef}>
-                        <div>Completed</div>
-                        {completed.map((todo, index) => (
-                            <TodoCard todo={todo} key={todo.id} index={index}/>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                  }
-              </Droppable>
-          </DragDropContext>
+          Hello
+          {console.log(lists)}
       </div>
-      </div>
+    </div>
   );
-};
+}
 
 // Used to get data from server
 // Filter the project that matches the context.params.projectId from the database
 // need to use axios/async function
 export const getServerSideProps = async (context: any) => {
   resetServerContext()
-  const res = projectFixture;
-  const projectArr = res.filter(
-    (data) => data.projectId === parseInt(context.params.projectId)
-  ); 
-  const project = projectArr[0];
+  let client = new Client({
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  });  
+  client.connect((err: Error) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('connected to PostgreSQL');
+    }
+  });
+  // const res = projectFixture;
+  // const projectArr = res.filter(
+  //   (data) => data.projectId === parseInt(context.params.projectId)
+  // ); 
+  // const project = projectArr[0];
+  // return {
+  //   props: {
+  //     project,
+  //   },
+  // };
+  let lists;
+  const getLists = new Promise((resolve, reject) => {
+    resolve(client.query('SELECT * FROM lists'))
+  })
+
+  await getLists
+    .then((results: any) => {
+      lists = results.rows
+      console.log(lists)
+    })
   return {
     props: {
-      project,
-    },
-  };
+      lists: lists
+    }
+  }
 };
 // make new list component
-// in here, get lists that correspond to the project id
+// in here, GET lists that correspond to the project id
 // THEN (in another component) get cards that correspond to the list id
 // finally, add logic for moving cards
 
 export default Project;
 
+// DragDropContext onDragEnd={handleDragEnd}>
+//               {/* Each droppable should become its own list component with a name */}
+//               {/* Map out ALL lists here (add a max amount?)*/}
+//               <Droppable droppableId="todos">
+//                   {(provided) => 
+//                     <div className={styles.todos} {...provided.droppableProps} ref={provided.innerRef}>
+//                         <div>To Do</div>
+//                         {provided.placeholder}
+//                     </div>
+//                   }
+//               </Droppable>
+//               <Droppable droppableId="in-progress">
+//                   {(provided) => 
+//                     <div className={styles.todos} {...provided.droppableProps} ref={provided.innerRef}>
+//                         <div>In Progress</div>
+//                         {provided.placeholder}
+//                     </div>
+//                   }
+//               </Droppable>
+//               <Droppable droppableId="completed">
+//                   {(provided) => 
+//                     <div className={styles.todos} {...provided.droppableProps} ref={provided.innerRef}>
+//                         <div>Completed</div>
+//                         {completed.map((todo, index) => (
+//                             <TodoCard todo={todo} key={todo.id} index={index}/>
+//                         ))}
+//                         {provided.placeholder}
+//                     </div>
+//                   }
+//               </Droppable>
+//           </DragDropContext>
